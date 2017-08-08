78339d76d5f7782992e178f51853b5748accb6fe
Jerome <jscar216@gmail.com>
2017-08-08 11:48:09 -0400

    Fixed duplicate number

--- Jackpot/Scripts/script.js
+++ Jackpot/Scripts/script.js
@@ -11,8 +11,23 @@
 function getPBNums() {
     document.getElementById("PB1").innerHTML = getRando(65);
     document.getElementById("PB2").innerHTML = getRando(65);
+    while (document.getElementById("PB2").innerHTML == document.getElementById("PB1").innerHTML) {
+        document.getElementById("PB2").innerHTML = getRando(65);
+    }
     document.getElementById("PB3").innerHTML = getRando(65);
+    while (document.getElementById("PB3").innerHTML == document.getElementById("PB1").innerHTML || document.getElementById("PB3").innerHTML == document.getElementById("PB2").innerHTML) {
+        document.getElementById("PB3").innerHTML = getRando(65);
+    }
     document.getElementById("PB4").innerHTML = getRando(65);
+    while (document.getElementById("PB4").innerHTML == document.getElementById("PB1").innerHTML || document.getElementById("PB4").innerHTML == document.getElementById("PB2").innerHTML || document.getElementById("PB4").innerHTML == document.getElementById("PB3").innerHTML) {
+        document.getElementById("PB4").innerHTML = getRando(65);
+    }
     document.getElementById("PB5").innerHTML = getRando(65);
+    while (document.getElementById("PB5").innerHTML == document.getElementById("PB1").innerHTML || document.getElementById("PB5").innerHTML == document.getElementById("PB2").innerHTML || document.getElementById("PB5").innerHTML == document.getElementById("PB3").innerHTML || document.getElementById("PB5").innerHTML == document.getElementById("PB4").innerHTML) {
+        document.getElementById("PB4").innerHTML = getRando(65);
+    }
     document.getElementById("PB6").innerHTML = getRando(65);
+    while (document.getElementById("PB6").innerHTML == document.getElementById("PB1").innerHTML || document.getElementById("PB6").innerHTML == document.getElementById("PB2").innerHTML || document.getElementById("PB6").innerHTML == document.getElementById("PB3").innerHTML || document.getElementById("PB6").innerHTML == document.getElementById("PB4").innerHTML || document.getElementById("PB6").innerHTML == document.getElementById("PB5").innerHTML)  {
+        document.getElementById("PB4").innerHTML = getRando(65);
+    }
 }
